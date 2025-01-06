96075fe818a8af027c82886f6741cd29c8cd3547
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-06 11:27:54 -0500

    Added console input

--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 
 int main()
 {
-	printf("C style hello\n");
-	std::cout << "C++ style hello" << std::endl;
+	int favouriteNumber = -1;
+	std::cout << "What is your favourite number?\n";
+	std::cin >> favouriteNumber;
+	//std::cout << "Your favourite number is: " << favouriteNumber << std::endl;
+
+	// C equivalent of the above output (%i means integer)
+	printf("Your favourite number is: %i\n", favouriteNumber);
+	//printf("C style hello\n");
+	//std::cout << "C++ style hello" << std::endl;
+
 	return 0;
 }
