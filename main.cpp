d6f96fc7fdfd5117497a7c65259ed4c93c24af09
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-06 11:32:10 -0500

    Removed code from master

--- main.cpp
+++ main.cpp
@@ -1,16 +1,5 @@
-#include <iostream>
-
+// Simplest c++ program:
 int main()
 {
-	int favouriteNumber = -1;
-	std::cout << "What is your favourite number?\n";
-	std::cin >> favouriteNumber;
-	//std::cout << "Your favourite number is: " << favouriteNumber << std::endl;
-
-	// C equivalent of the above output (%i means integer)
-	printf("Your favourite number is: %i\n", favouriteNumber);
-	//printf("C style hello\n");
-	//std::cout << "C++ style hello" << std::endl;
-
 	return 0;
 }
