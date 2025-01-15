a1e50b4f61ef404e098f2870a29b8273ee820031
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-15 08:25:06 -0500

    Asked for user input

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
-// Simplest c++ program:
+#include <iostream>
+
+// -Stores players
+// -Each player can have multiple weapons
+// -Each weapon can have multiple attachments 
 int main()
 {
+	int playerCount = 0;
+	printf("Enter the number of players.\n");
+	std::cin >> playerCount;
+	printf("The game has %i players.\n", playerCount);
 	return 0;
 }
