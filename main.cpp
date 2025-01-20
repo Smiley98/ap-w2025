152a30b00790280db3474b52fe4de67dad0590d0
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-20 11:03:43 -0500

    Added memory corruption example

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 
-int main()
+void PointerExample()
 {
 	// Variable's value vs variable's address (its location in memory, stored as a pointer).
 	int number = 5;
 	int* numberAddress = &number; // '&' means "Address of" --> where the variable is stored in memory.
 	printf("Value of number: %i\nAddress of number: %p\n", number, numberAddress);
+
+	// "Memory corruption" --> The compiler owns numberAddress, so by deleting it ourselves we break the rules (and our program crashes)!
+	//delete numberAddress;
+}
+
+int main()
+{
+	PointerExample();
 
 	// Arrays are represented as a pointer to the first address.
 	// From there, we can offset based on the size of our array (why numbers[0] means first number)!
