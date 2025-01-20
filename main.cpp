2e9ab6a766608958fa2c3a19fd39f4955bfe270f
Smiley98 <smileysofficialcinema@gmail.com>
2025-01-20 10:48:42 -0500

    Added pointers and addresses example

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-// Simplest c++ program:
+#include <iostream>
+
 int main()
 {
+	// Variable's value vs variable's address (its location in memory, stored as a pointer).
+	int number = 5;
+	int* numberAddress = &number; // '&' means "Address of" --> where the variable is stored in memory.
+	printf("Value of number: %i\nAddress of number: %p\n", number, numberAddress);
+
+	// Arrays are represented as a pointer to the first address.
+	// From there, we can offset based on the size of our array (why numbers[0] means first number)!
+	int* numbers = new int[10];
+	for (int i = 0; i < 10; i++)
+	{
+		numbers[i] = i + 1;
+	}
 	return 0;
 }
